246d1d6fba488f0bc73cbeda0925dd430de0fcff
Angelos Oikonomopoulos <c@quasinomial.net>
2018-05-25 02:00:00 +0200

    Explain things

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,25 @@ fn exist_differences(results : &[DiffResult<String>]) -> bool {
 
 #[derive(Debug)]
 enum State<'a> {
+    // Customary diff behavior is to present any removes before immediately
+    // following adds, however lcs_diff returns adds before removes. So we
+    // set aside any consecutive adds and print them as soon as it's clear
+    // we've observed (and emitted) all immediately following removes.
     CollectingAdds(Vec<&'a str>),
 
-    // Collect common (context) lines _before_ a change:
-    // Hold on to the last N common lines we've seen.
+    // Hold on to the last N common lines we've seen, dump them
+    // as the preceeding context if a new change (addition/removal)
+    // is seen.
+    // We also need to prepend a separator if there were context
+    // lines we had to drop, so our state also includes the number
+    // of observed common lines while in this state.
     CollectingCommonsTail(usize, VecDeque<&'a str>),
 
-    // Collect common (context) lines _after_ a change:
     // Accumulate up to $context lines, emit them, then switch
     // to CollectingCommonsTail.
     CollectingCommonsCorked(VecDeque<&'a str>),
+
+    // Emit seen remove, while holding on to any pending adds (see above)
     SequentialRemoves(Vec<&'a str>),
 }
 
@@ -49,10 +58,15 @@ fn display_diff_unified(out : &mut Write,
                         diff : &Vec<DiffResult<String>>) -> io::Result<i32> {
     use State::*;
     if !exist_differences(&diff) {
-        return Ok (0);
+        return Ok (0); // Exit w/o producing any output
     }
     let context = 3;
+
+    // We always need to print out a hunk header when we start, so use
+    // the same code path, forcing a header to be printed by pretending
+    // we've observed lots of common lines already.
     let mut state = CollectingCommonsTail(context + 1, VecDeque::new());
+
     for d in diff {
         eprintln!("state = {:?}", state);
         eprintln!("processing diff result: {:?}", d);
@@ -61,24 +75,30 @@ fn display_diff_unified(out : &mut Write,
                 match d {
                     DiffResult::Added(a) => {
                         adds.push(&new_lines[a.new_index.unwrap()]);
-                        CollectingAdds(adds)
+                        CollectingAdds(adds) // Still collecting adds
                     },
                     DiffResult::Removed(r) => {
                         writeln!(out, "-{}", &old_lines[r.old_index.unwrap()])?;
+                        // Change states, holding on to the pending adds
                         SequentialRemoves(adds)
                     },
                     DiffResult::Common(c) => {
+                        // No adjacent removes, time to print out the adds
                         for pa in adds.drain(..) {
                             writeln!(out, "+{}", pa)?;
                         }
                         let mut commons = VecDeque::new();
                         commons.push_back(&new_lines[c.new_index.unwrap()][..]);
+                        // We've just seen a change; this needs to be followed by
+                        // some context lines.
                         CollectingCommonsCorked(commons)
                     },
                 }
             },
             CollectingCommonsTail(seen, mut commons) => {
                 match d {
+                    // If the state changes, print out the last N lines, possibly
+                    // preceeded by a header
                     DiffResult::Added(a) => {
                         if seen > context {
                             writeln!(out, "--")?;
@@ -108,37 +128,42 @@ fn display_diff_unified(out : &mut Write,
                     },
                 }
             },
-            CollectingCommonsCorked(mut commons_after) => {
+            CollectingCommonsCorked(mut commons) => {
                 match d {
+                    // State change -> print collected common lines
                     DiffResult::Added(a) => {
-                        for pc in commons_after.drain(..) {
+                        for pc in commons.drain(..) {
                             writeln!(out, " {}", pc)?;
                         }
                         let adds = vec![&new_lines[a.new_index.unwrap()][..]];
                         CollectingAdds(adds)
                     },
                     DiffResult::Removed(r) => {
-                        for pc in commons_after.drain(..) {
+                        for pc in commons.drain(..) {
                             writeln!(out, " {}", pc)?;
                         }
                         writeln!(out, "-{}", &old_lines[r.old_index.unwrap()])?;
                         SequentialRemoves(vec![])
                     },
                     DiffResult::Common(c) => {
-                        commons_after.push_back(&new_lines[c.new_index.unwrap()]);
-                        if commons_after.len() == context {
-                            for pc in commons_after.drain(..) {
+                        commons.push_back(&new_lines[c.new_index.unwrap()]);
+                        if commons.len() == context {
+                            // We've accumulated $context common lines after
+                            // a change; print them out, then start collecting
+                            // common lines to print _before_ the next change.
+                            for pc in commons.drain(..) {
                                 writeln!(out, " {}", pc)?;
                             }
                             CollectingCommonsTail(0, VecDeque::new())
                         } else {
-                            CollectingCommonsCorked(commons_after)
+                            CollectingCommonsCorked(commons)
                         }
                     },
                 }
             },
             SequentialRemoves(mut adds) => {
                 match d {
+                    // State change -> time to print out the pending adds
                     DiffResult::Added(a) => {
                         for pa in adds.drain(..) {
                             writeln!(out, "+{}", pa)?;
@@ -147,6 +172,7 @@ fn display_diff_unified(out : &mut Write,
                         CollectingAdds(adds)
                     },
                     DiffResult::Removed(r) => {
+                        // Simply print out the remove
                         writeln!(out, "-{}", &old_lines[r.old_index.unwrap()])?;
                         SequentialRemoves(adds)
                     },
@@ -167,17 +193,25 @@ fn display_diff_unified(out : &mut Write,
     eprintln!("Handling final state: {:?}", state);
     // Cleanup
     match state {
+        // We might end up here if the last additions are
+        // exactly at the end of the file.
         CollectingAdds (mut adds) => {
             for pa in adds.drain(..) {
                 writeln!(out, "+{}", pa)?;
             }
         },
-        CollectingCommonsTail(_, mut commons) => (),
+        // Those are common lines we collected in anticipation of the
+        // next change. No change is coming any more, so drop them here.
+        CollectingCommonsTail(_, _) => (),
+        // We'll get here if there were < $context common lines between
+        // the last change and the end of the file. We still need to
+        // print them.
         CollectingCommonsCorked(mut commons) => {
             for pc in commons.drain(..) {
                 writeln!(out, " {}", pc)?;
             }
         },
+        // We may end up here if the last change is at the EOF.
         SequentialRemoves(mut adds) => {
             for pa in adds.drain(..) {
                 writeln!(out, "+{}", pa)?;
