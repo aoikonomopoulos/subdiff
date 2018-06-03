bdf4aeba8dbd1b54e46cbf3ce63c8fc4cc5423fe
Angelos Oikonomopoulos <c@quasinomial.net>
2018-06-03 07:00:00 +0200

    Begin implementing support for multiple regexps

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ use std::fs::File;
 use std::path::Path;
 use std::process::exit;
 use std::str::FromStr;
-use clap::{App, Arg};
-use regex::bytes::Regex;
+use clap::{App, Arg, Values};
+use regex::bytes::{Regex, RegexSet};
 
 macro_rules! dprintln {
     ($dbg:expr, $fmt:expr, $( $args:expr ),*) => {
@@ -59,45 +59,105 @@ fn exist_differences<T : PartialEq + Clone>(results : &[DiffResult<T>]) -> bool 
                        })
 }
 
-fn extract_re_matches(conf : &Conf, re : &mut Regex, line : &[u8]) -> Vec<u8> {
-    let mut ret = vec![];
-    match re.captures(line) {
-        Some (caps) => {
-            for i in 1..caps.len() {
-                let m = &caps[i];
-                dprintln!(conf.debug, "Got match: `{}`",
-                          String::from_utf8(m.to_vec()).unwrap());
-                ret.write_all(m).unwrap();
-            }
-        },
-        None => {
-            ret.write_all(line).unwrap();
+struct MultiRe {
+    multi : RegexSet,
+    regexes : Vec<Regex>,
+}
+
+impl MultiRe {
+    fn build<I, S>(strs : I) -> MultiRe
+    where S : AsRef<str>,
+    I : IntoIterator<Item = S> + Clone
+    {
+        // Compile the individual REs first, so that we can tell
+        // the user which RE had an error.
+        let regexes =
+            strs.clone().into_iter().map(|s| match Regex::new(s.as_ref()) {
+                Ok (re) => re,
+                Err (err) => {
+                    eprintln!("Could not compile regular expression `{}`: {}",
+                              s.as_ref(), err);
+                    exit(2)
+                },
+            }).collect();
+        let multi = match RegexSet::new(strs) {
+            Ok (set) => set,
+            Err (err) => {
+                eprintln!("Could not build regular expression set: {}", err);
+                exit(2)
+            },
+        };
+        MultiRe {
+            multi,
+            regexes,
         }
     }
-    ret
+    fn sub(&self, conf : &Conf, line : &[u8]) -> Option<Vec<u8>> {
+        let mut matches = self.multi.matches(line).into_iter();
+        match matches.next() {
+            None => None,
+            Some (single) => {
+                match matches.next() {
+                    None => {
+                        let mut ret = vec![];
+                        let re = &self.regexes[single];
+                        if let Some (caps) = re.captures(line) {
+                            for i in 1..caps.len() {
+                                match caps.get(i) {
+                                    Some (m) => {
+                                        dprintln!(conf.debug, "Got match[{}]: `{}`", i,
+                                                  String::from_utf8(m.as_bytes().to_vec()).unwrap());
+                                        ret.write_all(m.as_bytes()).unwrap();
+                                    },
+                                    None => {
+                                        dprintln!(conf.debug, "No match[{}]", i);
+                                    }
+                                }
+                            }
+                        } else {
+                            panic!("RegexSet claimed a match, but the RE disagrees")
+                        }
+                        Some (ret)
+                    },
+                    Some (_) => {
+                        eprintln!("Line is matched by more than \
+                                   one regular expression:");
+                        io::stderr().write_all(b"`").unwrap();
+                        io::stderr().write_all(line).unwrap();
+                        eprintln!("` is matched by:");
+                        for re in &self.regexes {
+                            eprintln!("{}", re);
+                        }
+                        exit(2)
+                    }
+                }
+            }
+        }
+    }
 }
 
-fn pick_lines(conf : &Conf, mut re : &mut Regex, lines : &[Vec<u8>]) -> Vec<Vec<u8>> {
-    lines.iter().map(|l| extract_re_matches(conf, &mut re, l)).collect()
+fn extract_re_matches(conf : &Conf, re : &mut MultiRe, line : &[u8]) -> Vec<u8> {
+    match re.sub(conf, line) {
+        None => line.to_vec(),
+        Some (s) => s,
+    }
 }
 
-fn diff_files(out : &mut Write, conf : &Conf, re : Option<&str>,
+fn pick_lines(conf : &Conf, mut mre : &mut MultiRe, lines : &[Vec<u8>]) -> Vec<Vec<u8>> {
+    lines.iter().map(|l| { extract_re_matches(conf, &mut mre, l)
+    }).collect()
+}
+
+fn diff_files(out : &mut Write, conf : &Conf, re : Option<Values>,
               old : &Path, new : &Path) -> io::Result<i32> {
     let old_lines = read_lines(old)?;
     let new_lines = read_lines(new)?;
 
     let diff : Vec<DiffResult<Vec<u8>>> = match re {
-        Some (re) => {
-            let mut re = match Regex::new(re) {
-                Ok (re) => re,
-                Err (err) => {
-                    eprintln!("Could not compile regular expresssion `{}`: {}",
-                              "XXX", err);
-                    exit(2)
-                }
-            };
-            lcs_diff::diff(&pick_lines(conf, &mut re, &old_lines),
-                           &pick_lines(conf, &mut re, &new_lines))
+        Some (values) => {
+            let mut mre = MultiRe::build(values);
+            lcs_diff::diff(&pick_lines(conf, &mut mre, &old_lines),
+                           &pick_lines(conf, &mut mre, &new_lines))
         },
         None => lcs_diff::diff(&old_lines, &new_lines)
     };
@@ -139,6 +199,8 @@ fn main() {
              .short("r")
              .long("regex")
              .takes_value(true)
+             .multiple(true)
+             .number_of_values(1)
              .value_name("RE")
              .help("Compare the parts of lines matched by this regexp"))
         .get_matches();
@@ -150,7 +212,7 @@ fn main() {
     };
     let ecode = match diff_files(&mut io::stdout(),
                                  &conf,
-                                 matches.value_of("common_re"),
+                                 matches.values_of("common_re"),
                                  Path::new(matches.value_of("old").unwrap()),
                                  Path::new(matches.value_of("new").unwrap())) {
         Ok (ecode) => ecode,
